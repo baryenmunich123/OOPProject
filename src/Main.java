f05499cf6db71b6448df0039dede5e054a681cd7
RexeonNova <hoclamhuynh@gmail.com>
2020-11-28 19:58:33 +0700

    Main Program

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,14 @@ import java.io.IOException;
 import java.awt.event.KeyEvent;
 
 public class Main{
-	gamePanel game;
+	GamePanel game;
 	public Main() {
 		JFrame f = new JFrame("Megaman 1");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Toolkit toolkit = f.getToolkit();
         Dimension screen = toolkit.getScreenSize();
 		f.setBounds(screen.width/2 - 500, screen.height/2 - 300, 1000, 600);
-		game = new gamePanel();
+		game = new GamePanel();
 		f.add(game);
 		f.setVisible(true);
 	}

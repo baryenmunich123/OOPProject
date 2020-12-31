14929c84ce32debfcb03f6ef4c318fac16af5378
ITITIU19020 <khoahaminh2001@gmail.com>
2020-12-31 09:28:34 +0700

    load data

--- src/CacheDataLoader.java
+++ src/CacheDataLoader.java
@@ -1,30 +1,12 @@
 package src;
 
-
-
-
-import java.applet.Applet;
-import java.applet.AudioClip;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Hashtable;
 import javax.imageio.ImageIO;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author phamn
- */
 public class CacheDataLoader {
     
     private static CacheDataLoader instance = null;

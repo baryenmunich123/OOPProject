f4e94a868329d3ba8c203cee4e0ee97ee067f46e
RexeonNova <hoclamhuynh@gmail.com>
2021-01-06 23:31:17 +0700

    fragaergae

--- src/GameWorldState.java
+++ src/GameWorldState.java
@@ -37,20 +37,24 @@ public class GameWorldState {
 	public void initializeEnemy() {
 		Enemy e1 = new StandingEnemy(1100, 482, this);
 		Enemy_Manager.addEnemy(e1);
-		Enemy e2 = new StandingEnemy(1500, 482, this);
+		Enemy e2 = new StandingEnemy(3500, 720, this);
 		Enemy_Manager.addEnemy(e2);
-		Enemy e3 = new StandingEnemy(2840, 630, this);
+		Enemy e3 = new StandingEnemy(2840, 650, this);
 		Enemy_Manager.addEnemy(e3);
 		Enemy e4 = new StandingEnemy(5000, 600, this);
 		Enemy_Manager.addEnemy(e4);
-		Enemy e5 = new FlyEnemy(500,400,this);
+		Enemy e5 = new FlyEnemy(1800,750,this);
 		Enemy_Manager.addEnemy(e5);
-		Enemy e6 = new FlyEnemy(1000,400,this);
+		Enemy e6 = new FlyEnemy(2400,600,this);
 		Enemy_Manager.addEnemy(e6);
-		Enemy e7 = new FlyShootEnemy(300,290,this);
+		Enemy e7 = new FlyShootEnemy(2000,400,this);
 		Enemy_Manager.addEnemy(e7);
-		Enemy e8 = new ShortRangeEnemy(300,482,this);
+		Enemy e8 = new ShortRangeEnemy(1100,300,this);
 		Enemy_Manager.addEnemy(e8);
+		Enemy e9 = new ShortRangeEnemy(3500, 600, this);
+		Enemy_Manager.addEnemy(e9);
+		Enemy e10 = new FlyShootEnemy(2800,400,this);
+		Enemy_Manager.addEnemy(e10);
 	}
 
 	public void Update() {
--- src/ShortRangeEnemy.java
+++ src/ShortRangeEnemy.java
@@ -5,11 +5,11 @@ import java.awt.*;
 
 public class ShortRangeEnemy extends Enemy {
     private Image img;
-    private int speedX = 5, speedY = 5;
+    private int speedX = -3, speedY = 5;
     public ShortRangeEnemy(double startX, double startY, GameWorldState gameWorld) {
         super(startX, startY, 50, 50, gameWorld);
         setDamage(3);
-        setHP(5);
+        setHP(3);
         ImageIcon i = new ImageIcon("Image/Enemy4.png");
         img = i .getImage();
     }
@@ -28,23 +28,57 @@ public class ShortRangeEnemy extends Enemy {
     @Override
     public void draw(Graphics2D g2) {
         g2.drawImage(img, (int) super.getStartX() - (int) super.getGameWorld().camera.getX() - 45,
-                (int) super.getStartY() - (int) super.getGameWorld().camera.getY() - 50, null);
+                (int) super.getStartY() - (int) super.getGameWorld().camera.getY() - 50 + 5, null);
     }
 
     @Override
     public void updateState() {
         super.updateState();
 
-//        super.setStartX(super.getStartX() + speedX);
-//        if (super.getGameWorld().physicalMap.haveCollisionWithLeftWall(super.getBoundForCollisionWithMap()) != null) {
-//            Rectangle r1 = super.getGameWorld().physicalMap.haveCollisionWithLeftWall(this.getBoundForCollisionWithEnemy());
-//            super.setStartX((float)r1.getX() + (float)r1.getWidth());
-//            //speedX = 0;
-//        }
-//        if (super.getGameWorld().physicalMap.haveCollisionWithRightWall(super.getBoundForCollisionWithMap()) != null) {
-//            Rectangle r2 = super.getGameWorld().physicalMap.haveCollisionWithRightWall(this.getBoundForCollisionWithEnemy());
-//            super.setStartX((float)r2.getX() - (float)(r2.getWidth()));
-//            //speedX = 0;
-//        }
+        super.setStartX(super.getStartX() + speedX);
+        super.setStartY(super.getStartY() + speedY);
+        if (super.getGameWorld().physicalMap.haveCollisionWithLeftWall(getLeftRect()) != null) {
+            Rectangle r1 = super.getGameWorld().physicalMap.haveCollisionWithLeftWall(getLeftRect());
+            super.setStartX((float)r1.getX() + (float)r1.getWidth());
+            speedX = 3;
+        }
+        if (super.getGameWorld().physicalMap.haveCollisionWithRightWall(getRightRect()) != null) {
+            Rectangle r2 = super.getGameWorld().physicalMap.haveCollisionWithRightWall(getRightRect());
+            super.setStartX((float)r2.getX() - (float)(super.getWidth()));
+            speedX = -3;
+        }
+        if (super.getGameWorld().physicalMap.haveCollisionWithTop(getUpRect()) != null) {
+			Rectangle r3 = super.getGameWorld().physicalMap.haveCollisionWithTop(getUpRect());
+			speedY = 0;
+			setStartY((float)r3.getY() + super.getGameWorld().physicalMap.getTileSize());
+		}
+		if (super.getGameWorld().physicalMap.haveCollisionWithLand(getDownRect()) != null) {
+			Rectangle r4 = super.getGameWorld().physicalMap.haveCollisionWithLand(getDownRect());
+			speedY = 0;
+			setStartY((float)r4.getY() - (float)super.getHeight() + 5);
+		}
+		else {
+				speedY = 7;
+		}
     }
+    public Rectangle getLeftRect() {
+		Rectangle left = new Rectangle();
+		left.setBounds((int)super.getStartX(), (int)super.getStartY() + 1, 3, (int)super.getHeight() - 5);
+		return left;
+	}
+	public Rectangle getRightRect() {
+		Rectangle right = new Rectangle();
+		right.setBounds((int)super.getStartX() + (int)super.getWidth() - 3, (int)super.getStartY() + 1, 3, (int)super.getHeight() - 5);
+		return right;
+	}
+	public Rectangle getDownRect() {
+		Rectangle down = new Rectangle();
+		down.setBounds((int)super.getStartX() + 1, (int)super.getStartY() + (int)super.getHeight() -1, (int)super.getWidth() - 2, 3);
+		return down;
+	}
+	public Rectangle getUpRect() {
+		Rectangle up = new Rectangle();
+		up.setBounds((int)super.getStartX() + 1, (int)super.getStartY() - 5, (int)super.getWidth() - 2, 8);
+		return up;
+	}
 }
